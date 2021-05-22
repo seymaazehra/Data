463d5323b564b7a5adb86de56e034e777298170d
seymaazehra <75252489+seymaazehra@users.noreply.github.com>
2021-05-22 20:27:07 +0300

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,102 @@
+package inheritance2;
+
+//iþVerenler
+
+-- Table: public.web sitesi
+
+-- DROP TABLE public."web sitesi";
+
+CREATE TABLE public."web sitesi"
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."web sitesi"
+    OWNER to postgres;
+
+
+-- Table: public.þirket adý
+
+-- DROP TABLE public."þirket adý";
+
+CREATE TABLE public."þirket adý"
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."þirket adý"
+    OWNER to postgres;
+
+//HRMS
+-- Table: public.Ad-Soyad
+
+-- DROP TABLE public."Ad-Soyad";
+
+CREATE TABLE public."Ad-Soyad"
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Ad-Soyad"
+    OWNER to postgres;
+
+
+-- Table: public.EPosta
+
+-- DROP TABLE public."EPosta";
+
+CREATE TABLE public."EPosta"
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."EPosta"
+    OWNER to postgres;
+
+
+-- Table: public.Sifre
+
+-- DROP TABLE public."Sifre";
+
+CREATE TABLE public."Sifre"
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."Sifre"
+    OWNER to postgres;
+
+
+
+-- Table: public.SifreTekrarý
+
+-- DROP TABLE public."SifreTekrarý";
+
+CREATE TABLE public."SifreTekrarý"
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."SifreTekrarý"
+    OWNER to postgres;
+
+
+
+-- Table: public.TC
+
+-- DROP TABLE public."TC";
+
+CREATE TABLE public."TC"
+(
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public."TC"
+    OWNER to postgres;
